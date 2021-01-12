a60b845f878227ca71264c9fa5678f54b645f16f
pabloa1311 <pabloa1311@gmail.com>
2021-01-12 07:29:46 -0500

    first components

---
+++ src/containers/jumbotron.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Jumbotron from '../components/jumbotron/index';
+import jumboData from '../fixtures/jumbo.json';
+
+export function JumbotronContainer() {
+    return (
+        <Jumbotron.Container>
+        {jumboData.map((item) => (
+            <Jumbotron key={item.id} direction={item.direction}>
+                <Jumbotron.Title>{item.title}</Jumbotron.Title>
+                <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+                <Jumbotron.Image src={item.image} alt={item.alt} />
+            </Jumbotron>
+        ))}
+        </Jumbotron.Container>
+    )
+}
